daf0e7906fe5d52252a222b7d8c778a5799ff08d
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-05-03 17:11:04 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+var api = "http://sg1.npool.pw:3010";
+
+var api_blockexplorer = "http://165.227.125.101:3009";
+
+var poolHost = "sg1.npool.pw";
+
+var irc = "irc.freenode.net/#triton";
+
+var email = "support@tritonproject.org";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://explorer.tritonproject.org/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://explorer.tritonproject.org/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "trit": [
+        ["pool.tritonproject.org", "http://165.227.125.101:8117"],
+        ["triton.fatpanda.club", "http://54.148.101.99:8018"],
+        ["luckypool.io", "https://trit.luckypool.io/api/pool"],
+        ["trit.newpool.pw", "http://trit.newpool.pw:8120"],
+        ["cryptoknight.cc/triton/","https://cryptoknight.cc/rpc/triton"],
+        ["npool.pw/triton/","http://sg1.npool.pw:3010"]
+    ]
+};
